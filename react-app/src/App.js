12fb4e4ece9f88436d6bd8e30dd656a9379ec915
Bonik <bonislavskys@gmail.com>
2024-03-03 01:43:59 +0200

    добавил Material UI

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,12 @@
-
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { StyledEngineProvider } from '@mui/material/styles';
+import BasicGrid from './BasicGrid';
 
 function App() {
   return (
     <div className="App">
-          gfdgfdfdggdfgdf
+         <BasicGrid />
     </div>
   );
 }
